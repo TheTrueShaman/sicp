89d80747f2fe337fd3d9ca2106dadaed44f4a192
TheTrueShaman <68354378+The-Shaman@users.noreply.github.com>
2022-01-06 21:04:53 -0800

    Create Exercise1.19.scm

---
+++ Exercise1.19.scm
@@ -0,0 +1,22 @@
+; This fibonacci function solves the function in a logarithmic amount of time.
+(define (fib n)
+	(fib-iter 1 0 0 1 n))
+
+(define (fib-iter a b p q count)
+  (cond ((= count 0)
+         b)
+        ((even? count)
+         (fib-iter a
+                   b
+                   (+ (* p p) (* q q))    ;compute p'
+                   (+ (* 2 q p) (* q q))  ;compute q'
+                   (/ count 2)))
+        (else
+         (fib-iter (+ (* b q)
+                      (* a q)
+                      (* a p))
+                   (+ (* b p)
+                      (* a q))
+                   p
+                   q
+                   (- count 1)))))
