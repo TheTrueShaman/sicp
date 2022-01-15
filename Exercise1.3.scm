0729dde2f09d83f8633ef7755a2212847f1e97d6
TheTrueShaman <68354378+The-Shaman@users.noreply.github.com>
2022-01-15 12:15:30 -0800

    Update and rename Exercise1.3 to Exercise1.3.scm

--- Exercise1.3.scm
+++ Exercise1.3.scm
@@ -6,5 +6,6 @@
 
 (define (min x y) (if (< x y) x y))
 
+; Takes 3 inputs and returns the sum of squares of the two largest numbers.
 (define (sum-of-squares-of-two-largest x y z)
         (sum-of-squares (max x y) (max z (min x y))))
