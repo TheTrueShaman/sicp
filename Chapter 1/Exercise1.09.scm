34957bbc78d3b5c9ee528295b1c00e60e1536c0a
TheTrueShaman <68354378+The-Shaman@users.noreply.github.com>
2022-01-16 12:21:28 -0800

    Create Exercise1.09.scm

---
+++ Chapter 1/Exercise1.09.scm
@@ -0,0 +1,33 @@
+;For both of the following functions, we will illustrate the process taken in evaluating (+ 4 5) and determine whether they are iterative or recursive.
+
+;Function one:
+(define (+ a b)
+  (if (= a 0)
+      b
+      (inc (+ (dec a) b))))
+
+;(+ 4 5)
+;(inc (+ 3 5))
+;(inc (inc (+ 2 5)))
+;(inc (inc (inc (+ 1 5))))
+;(inc (inc (inc (inc 5))))
+;(inc (inc (inc 6)))
+;(inc (inc 7))
+;(inc 8)
+;Value: 9
+
+;The process described above by function one is recursive.
+
+;Function two:
+(define (+ a b)
+  (if (= a 0)
+      b
+      (+ (dec a) (inc b))))
+
+;(+ 4 5)
+;(+ 3 6)
+;(+ 2 7)
+;(+ 1 8)
+;Value: 9
+
+;The process described above by function two is recursive.
