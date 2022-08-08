799514b9308bb92f471841b71d831a595e240b11
TheTrueShaman <68354378+The-Shaman@users.noreply.github.com>
2022-08-08 13:18:48 -0700

    Create Exercise1.36.scm

---
+++ Chapter 1/Exercise1.36.scm
@@ -0,0 +1,18 @@
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (display guess) (newline)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+;This is without damping. It takes 35 steps to complete
+(display (fixed-point (lambda (x) (/ (log 1000) (log x))) 2)) (newline)
+
+;This is with damping. It takes 10 steps to complete. In this case, damping is useful and increases speed of convergance.
+(display (fixed-point (lambda (x) (/ (log 1000) (log x))) 2)) (newline)
